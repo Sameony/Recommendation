3520a4160af38dd24afc6fef9c84932f46e6d658
Sameony <80711125+Sameony@users.noreply.github.com>
2021-09-09 14:16:52 +0530

    Functioning list Onclick list functioning

--- src/App.js
+++ src/App.js
@@ -1,48 +1,95 @@
 import "./styles.css";
 import React, { useState } from "react";
 
-var list = ["Action", "Fantasy", "Comedy"];
-var FantasyList = [
-  "FullMetal Alchemist",
-  "Hunter x Hunter",
-  "Fate/Zero",
-  "Irregular of the magic high school"
-];
-var ActionList = [
-  "Parasyte Maxim",
-  "Tokyo Ghoul",
-  "Akame Ga Kill",
-  "Fate/Zero"
-];
-var ComedyList = [
-  "Gintama",
-  "Grand Blue",
-  "Daily lives of highschool boys",
-  "One punch Man"
-];
+const Animes = {
+  Action: [
+    {
+      name: "Tokyo Ghoul",
+      rating: 6.4
+    },
+    {
+      name: "Akame Ga Kill",
+      rating: 7.4
+    },
+    {
+      name: "Fate/Zero",
+      rating: 5.7
+    },
+    {
+      name: "Parasyte Maxim",
+      rating: 8.8
+    }
+  ],
+  Comedy: [
+    {
+      name: "Gintama",
+      rating: 8.5
+    },
+    {
+      name: "Grand blue",
+      rating: 8.7
+    },
+    {
+      name: "Daily lives of high school boys",
+      rating: 7.5
+    },
+    {
+      name: "One punch man",
+      rating: 7.5
+    }
+  ],
+  Fantasy: [
+    {
+      name: "FullMetal Alchemist",
+      rating: 7.9
+    },
+    {
+      name: "Hunter x Hunter",
+      rating: 4.2
+    },
+    {
+      name: "Fate/Zero",
+      rating: 5.8
+    },
+    {
+      name: "Irregular of the magic high school",
+      rating: 8.8
+    }
+  ]
+};
 
 export default function App() {
-  function clickEventHandler(Tag) {
-    if (Tag === "Action") {
-    }
-    if (Tag === "Comedy") {
-    }
-    if (Tag === "Fantasy") {
-    }
+  const [anime, setAnime] = useState("Action");
+
+  function ClickEventHandler(tag) {
+    setAnime(tag);
   }
   return (
     <div className="App">
       <h1> Recommendation </h1>
       <nav>
-        {list.map(function (item) {
-          return (
-            <span className="pointer" onClick={() => clickEventHandler(item)}>
-              {" "}
-              {item}{" "}
-            </span>
-          );
-        })}
+        <ul>
+          {Object.keys(Animes).map((tag) => (
+            <li
+              key={tag}
+              className="pads navigate"
+              onClick={() => ClickEventHandler(tag)}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
       </nav>
+
+      <div>
+        {Animes[anime].map((item) => (
+          <li key={item}>
+            <div>
+              {item.name} {item.rating}
+            </div>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
