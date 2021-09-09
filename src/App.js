6ffe1c2302cbb5aef32f40bf9bba8a40bdc3b897
Sameony <80711125+Sameony@users.noreply.github.com>
2021-09-09 13:31:11 +0530

    Displayed list of tags

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import "./styles.css";
 import React, { useState } from "react";
 
+var list = ["Action", "Fantasy", "Comedy"];
+var FantasyList = [
+  "FullMetal Alchemist",
+  "Hunter x Hunter",
+  "Fate/Zero",
+  "Irregular of the magic high school"
+];
+var ActionList = [
+  "Parasyte Maxim",
+  "Tokyo Ghoul",
+  "Akame Ga Kill",
+  "Fate/Zero"
+];
+var ComedyList = [
+  "Gintama",
+  "Grand Blue",
+  "Daily lives of highschool boys",
+  "One punch Man"
+];
+
 export default function App() {
+  function clickEventHandler(Tag) {
+    if (Tag === "Action") {
+    }
+    if (Tag === "Comedy") {
+    }
+    if (Tag === "Fantasy") {
+    }
+  }
   return (
     <div className="App">
       <h1> Recommendation </h1>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      <nav>
+        {list.map(function (item) {
+          return (
+            <span className="pointer" onClick={() => clickEventHandler(item)}>
+              {" "}
+              {item}{" "}
+            </span>
+          );
+        })}
+      </nav>
     </div>
   );
 }
