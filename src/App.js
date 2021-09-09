e4998693a1ede6c5186afb9ddc77d3e1856f244a
Sameony <80711125+Sameony@users.noreply.github.com>
2021-09-09 16:10:56 +0530

    added button and css

--- src/App.js
+++ src/App.js
@@ -5,55 +5,55 @@ const Animes = {
   Action: [
     {
       name: "Tokyo Ghoul",
-      rating: 6.4
+      rating: 7.08
     },
     {
       name: "Akame Ga Kill",
-      rating: 7.4
+      rating: 8.5
     },
     {
       name: "Fate/Zero",
-      rating: 5.7
+      rating: 8.34
     },
     {
       name: "Parasyte Maxim",
-      rating: 8.8
+      rating: 8.37
     }
   ],
   Comedy: [
     {
       name: "Gintama",
-      rating: 8.5
+      rating: 8.95
     },
     {
       name: "Grand blue",
-      rating: 8.7
+      rating: 8.42
     },
     {
       name: "Daily lives of high school boys",
-      rating: 7.5
+      rating: 8.26
     },
     {
       name: "One punch man",
-      rating: 7.5
+      rating: 8.72
     }
   ],
   Fantasy: [
     {
       name: "FullMetal Alchemist",
-      rating: 7.9
+      rating: 9.16
     },
     {
       name: "Hunter x Hunter",
-      rating: 4.2
+      rating: 9.06
     },
     {
-      name: "Fate/Zero",
-      rating: 5.8
+      name: "JoJO's Bizarre Adventure",
+      rating: 8.5
     },
     {
-      name: "Irregular of the magic high school",
-      rating: 8.8
+      name: "Assassination Classroom",
+      rating: 8.52
     }
   ]
 };
@@ -66,26 +66,30 @@ export default function App() {
   }
   return (
     <div className="App">
-      <h1> Recommendation </h1>
       <nav>
-        <ul>
-          {Object.keys(Animes).map((tag) => (
-            <li
-              key={tag}
-              className="pads navigate"
-              onClick={() => ClickEventHandler(tag)}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
+        <h1 className="Heading"> Anime Recommendations! </h1>
+
+        {Object.keys(Animes).map((tag) => (
+          <button
+            class="ListButton"
+            key={tag}
+            className="pads navigate"
+            onClick={() => ClickEventHandler(tag)}
+          >
+            {tag}
+          </button>
+        ))}
       </nav>
 
       <div>
         {Animes[anime].map((item) => (
           <li key={item}>
-            <div>
-              {item.name} {item.rating}
+            <div className="container">
+              <span className="listItems BorderMe">
+                Name:{item.name}
+                <br />
+                MAL rating: {item.rating}
+              </span>
             </div>
           </li>
         ))}
