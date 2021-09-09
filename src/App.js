df576b32025b19b211cdcc0fc78894e24f14844c
Sameony <80711125+Sameony@users.noreply.github.com>
2021-09-09 16:44:34 +0530

    Linked synopsis and images

--- src/App.js
+++ src/App.js
@@ -50,40 +50,46 @@ const Animes = {
     {
       name: "Daily lives of high school boys",
       rating: 8.26,
-      Img: "",
-      desc: ""
+      Img: "https://cdn.myanimelist.net/images/anime/3/33257.jpg",
+      desc:
+        "Roaming the halls of the all-boys Sanada North High School are three close comrades: the eccentric ringleader with a hyperactive imagination Hidenori, the passionate Yoshitake, and the rational and prudent Tadakuni."
     },
     {
       name: "One punch man",
       rating: 8.72,
-      Img: "",
-      desc: ""
+      Img: "https://cdn.myanimelist.net/images/anime/12/76049.jpg",
+      desc:
+        "The seemingly ordinary and unimpressive Saitama has a rather unique hobby: being a hero. In order to pursue his childhood dream, he trained relentlessly for three years—and lost all of his hair in the process. Now, Saitama is incredibly powerful, so much so that no enemy is able to defeat him in battle."
     }
   ],
   Fantasy: [
     {
       name: "FullMetal Alchemist",
       rating: 9.16,
-      Img: "",
-      desc: ""
+      Img: "https://cdn.myanimelist.net/images/anime/10/75815.jpg",
+      desc:
+        "Edward Elric, a young, brilliant alchemist, has lost much in his twelve-year life: when he and his brother Alphonse try to resurrect their dead mother through the forbidden act of human transmutation, Edward loses his brother as well as two of his limbs. With his supreme alchemy skills, Edward binds Alphonse's soul to a large suit of armor."
     },
     {
       name: "Hunter x Hunter",
       rating: 9.06,
-      Img: "",
-      desc: ""
+      Img: "https://cdn.myanimelist.net/images/anime/11/33657.jpg",
+      desc:
+        "Hunter x Hunter is set in a world where Hunters exist to perform all manner of dangerous tasks like capturing criminals and bravely searching for lost treasures in uncharted territories. Twelve-year-old Gon Freecss is determined to become the best Hunter possible in hopes of finding his father, who was a Hunter himself and had long ago abandoned his young son. However, Gon soon realizes the path to achieving his goals is far more challenging than he could have ever imagined."
     },
     {
       name: "JoJO's Bizarre Adventure",
       rating: 8.5,
-      Img: "",
-      desc: ""
+      Img: "https://cdn.myanimelist.net/images/anime/3/40409.jpg",
+      desc:
+        "Adapted from the first two arcs of Hirohiko Araki's outlandish manga series, JoJo's Bizarre Adventure follows the many thrilling expeditions of JoJo and his descendants. Whether it's facing off with the evil Dio, or combatting the sinister Pillar Men, there's always plenty of bizarre adventures in store."
     },
     {
       name: "Assassination Classroom",
       rating: 8.52,
-      Img: "",
-      desc: ""
+      Img: "https://cdn.myanimelist.net/images/anime/5/75639.jpg",
+      desc:
+        "When a mysterious creature chops the moon down to a permanent crescent, the students of class 3-E of Kunugigaoka Middle School find themselves confronted with an enormous task: assassinate the creature responsible for the disaster before Earth suffers a similar fate. However, the monster, dubbed Koro-sensei (the indestructible teacher), is able to fly at speeds of up to Mach 20, which he demonstrates freely, leaving any attempt to subdue him in his extraterrestrial dust. Furthermore, the misfits of 3-E soon find that the strange, tentacled beast is more than just indomitable—he is the best teacher they have ever had!"
     }
   ]
 };
