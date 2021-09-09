dc7af990a56514b61cd8cd2681d7fceede3b703f
Sameony <80711125+Sameony@users.noreply.github.com>
2021-09-09 13:08:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Recommendation </h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
